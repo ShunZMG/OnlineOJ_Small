aed77cbfde457394f9b6c2fed930df7b2baf3e79
Visual GMQ <2142587070@qq.com>
2019-06-29 18:08:34 +0800

    added signup show questions function

--- code/app.py
+++ code/app.py
@@ -1,13 +1,12 @@
 from flask import Flask, render_template, request, session, redirect, url_for
 from log2file import Log
 from dbcodes import *
+import json
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'this is secret key'
 
-def initApp():
-    session['dbManager'] = DBManager('onlineOJ', 'ComPro32API', 'localhost', 'root')
-    session['LOGINED'] = False
+dbmanager = DBManager('onlineOJ', 'ComPro32API', 'localhost', 'root')
 
 
 @app.route('/login', methods=["GET", "POST"])
@@ -17,11 +16,11 @@ def login():
     if request.method == 'POST':
         Log("recieved POST")
         Log("session['LOGINED']:%s" % session.get('LOGINED'))
-        if session['LOGINED'] == False:
+        if not session.get('LOGINED'):
+            session['LOGINED'] = False
             username = request.form['username']
             psd = request.form['password']
             Log("user logined\nusename:%s\npassword:%s" % (username, psd))
-            dbmanager = session['dbManager']
             dbmanager.m_useTable('Customers')
             if not dbmanager.m_itemExists(['username, psd'], 'username="%s" AND psd="%s"' % (username, psd)):
                 Log("登录失败")
@@ -34,12 +33,67 @@ def login():
     return render_template('login.html')
 
 
+@app.route("/signup", methods=['GET', 'POST'])
+def signup():
+    Log("进入signup界面")
+    if request.method == 'POST':
+        username = request.form['username']
+        psd = request.form['password']
+        Log("用户注册,username:%s, psd:%s" % (username, psd))
+        dbmanager.m_useTable("Customers")
+        if dbmanager.m_itemExists(['username', 'psd'], where="username='%s' AND psd='%s'" % (username, psd)):
+            Log("用户已经存在")
+            return render_template("signup.html", signuped=False)
+        session['LOGINED'] = True
+        dbmanager.m_insertItem([username, psd, 'NONE', 0, 0, 0])
+        Log("增加用户了")
+        return redirect(url_for("questions"))
+    return render_template("signup.html", signuped=True)
+
+
+@app.route("/questions")
+def questions():
+    Log("进入题目列表")
+    dbmanager.m_useTable("Questions")
+    questions = dbmanager.m_selectItem()
+    return render_template("QuestionBank.html",questions=questions)
+
+
+@app.route("/question/<Qname>")
+def writeQuestion(Qname):
+    Log("进入写题界面，题目：%s" % Qname)
+    dbmanager.logOn()
+    dbmanager.m_useTable("Questions")
+    Tpath = dbmanager.m_selectItem(['Tpath'], where="name='%s'" % Qname)[0]
+    global jsonfile
+    try:
+        jsonfile = open("./static/questions/%s" % Tpath)
+    except IOError:
+        Log("没有这个文件:%s" % Tpath)
+    file = json.load(jsonfile)
+    print(file['Context'])
+    return render_template("writeQuestion.html", name=Qname, context=file['Context'])
+
+
+@app.route("/competition")
+def competition():
+    Log("进入比赛界面")
+    return render_template("Competition.html")
+
+
+@app.route('/release')
+def release():
+    Log("进入Release页面")
+    return render_template('Release.html')
+
+
 @app.route('/', methods=['GET'])
 def homepage():
     Log("进入主页")
-    return render_template('homepage.html')
+    if not session.get('LOGINED'):
+        session['LOGINED'] = False
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
-    initApp()
     app.run(debug=True)
